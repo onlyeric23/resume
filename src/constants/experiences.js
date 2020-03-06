59bc51c42cd08a6c003b8abd4dc11f68e3e620b8
Eric <onlyeric23@gmail.com>
2020-03-06 19:04:08 +0800

    Fix the experience in New Garden

--- src/constants/experiences.js
+++ src/constants/experiences.js
@@ -2,11 +2,9 @@ export default [
   {
     name: "New Garden Co. Ltd., Front-end Engineer, Dec 2019 ~ Dec 2019",
     location: "Taipei, Taiwan",
-    desc: `● Developed a service platform to monitor vessels, schedule maintenance events, configure and watch alert notifications, and provide statistics.
-    ● Built a mobile application to monitor a vessel, watch alert notifications independently.
-    ● Built the corporate website from scratch.`,
+    desc: "● Developed a CMS platform.",
     subDesc: `Skills:
-    React, React Native, Gatsby, Redux, Redux Saga, D3, Google Maps, SEO, NodeJS, PostgreSQL, Sequelize, Serverless, AWS, Typescript, BLE, WebSocket, Git, Zeplin, Webpack, LESS`,
+    React, Redux, Firebase, SASS`,
   },
   {
     name: "Arasens Inc., Front-end Engineer, Apr 2019 ~ Oct 2019",
